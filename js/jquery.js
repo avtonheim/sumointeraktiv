fdc28395f497536d19da5728d6e1ed276bf47231
avtonheim <aleksandernygardtonheim@AleksandersMBP2.vimond.local>
2016-03-31 10:06:28 +0200

    fiksa småting med local storage

--- js/jquery.js
+++ js/jquery.js
@@ -1,69 +1,53 @@
-
-
-$(document).ready(function () {
-  $("#innhald").hide();
-  $("#aurora").hide();
-});
-
-//fikser å vise aurora etter å ha trykka play.
 $(document).ready(function(){
-$("#protovideo").on('play', function() {
-	        setTimeout(
-	            function () {
-                var aur =  $("#vanskelig").contents().find("#aurora") //for å finne aurora i iframe
-                $(aur).show();
-              }, 30000); //30 sek
-	    });
-});
+  $("#aurora").hide();
+  $("#skjorte").hide();
 
 
 //funksjon for å vise at ting blir lagt til i produktlista
-$(document).ready(function () {
   $("#detskjer1").hide();
-
   $("#før1").click(function () {
     $("#før1").hide();
     $("#detskjer1").fadeIn();
   });
-});
+
 
 //funksjon for å vise at ting blir lagt til i produktlista
 //Fikser animasjonting
-$(document).ready(function () {
-  $("#detskjer2").hide();
 
+  $("#detskjer2").hide();
   $("#før2").click(function () {
     $("#før2").hide();
     $("#detskjer2").fadeIn();
   });
-});
+
 
 
 //meny fiksing
-$(document).ready(function () {
   $(".container").hide();
-
   $("#hamburger-nav").click(function () {
     $(".container").toggle();
   });
-});
-
 
 
 //Fikser aktiv pause
-$(document).ready(function () {
+  $("#innhald").hide();
   $("#protovideo").on('pause', function() {
     $("#innhald").fadeIn(100);
-
   });
   $("#protovideo").on('play', function() {
     $("#innhald").fadeOut(100);
 
   });
+
+// Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("produkt", "1");
+
+} else {
+    document.getElementById("pop").innerHTML = "Sorry, your browser does not support Web Storage...";
+}
 });
-
-
-
 //fikser å legge inn tekst tima i videoen.
 document.addEventListener( "DOMContentLoaded", function() {
 
@@ -84,18 +68,42 @@ document.addEventListener( "DOMContentLoaded", function() {
            });
 
            popcorn.footnote({
-             start: 20,
-             end: 28,
+             start: 30,
+             end: 38,
              target: "#pop",
-             text: "Nå ser du at det nye albumet til Aurora er lagt til i produktlisten.",
+             text: "Nå ser du at det nye albumet til Aurora er lagt til i produktlisten."
            });
+
+           var pop = Popcorn( "#protovideo" );
+
+           //Legger til skjorte med id 1
+           pop.code({
+             start: 5, //5 sekund så kjem den fram
+             onStart: function( options ) {
+               //document.getElementById("pop").innerHTML = localStorage.getItem("produkt");
+               var skjorte = $("#vanskelig").contents().find("#skjorte");
+               $(skjorte).show();
+             }
+           });
+
+        //Legger til Aurora med id 2
+          pop.code({
+            start: 30, //30 sekund så kjem den fram.
+            onStart: function( options ) {
+              //document.getElementById("pop").innerHTML = localStorage.getItem("produkt");
+              var aur =  $("#vanskelig").contents().find("#aurora"); //for å finne aurora i iframe
+              $(aur).show();
+            }
+          });
+
 
 
            popcorn.footnote({
-             start: 29,
-             end: 35,
+             start: 40,
+             end: 48,
              target: "#pop",
              text: "Prøv å legge albumet til i huskelisten ved å klikke på stjernen!"
+
            });
 });
 
